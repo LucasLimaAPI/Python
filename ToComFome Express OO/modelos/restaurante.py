0ca7b8c68e1fa47282eb2f890cc223730edc9487
Lucas Lima de Oliveira <96830935+LucasLimaAPI@users.noreply.github.com>
2024-03-11 17:29:23 -0300

    Update restaurante.py

--- ToComFome Express OO/modelos/restaurante.py
+++ ToComFome Express OO/modelos/restaurante.py
@@ -1,7 +1,7 @@
 class Restaurante:
     restaurantes = []
 
-#__str__ = ele e um metoto especial que pega um objeto em formato de texto e mostra esta informação, a gente pode escolher o nome categoria o ativo a gente pode escolher e definir.
+#__str__ = ele e um metoto especial que pega um objeto em formato de texto e mostra esta informação, a gente pode escolher o nome categoria o ativo podemos pode escolher e definir.
 #__init__ = um método construtor sempre onde que criarmos uma instância de um objeto esse método é chamado
     
     def __init__(self, nome, categoria):#Para nao confundir sempre passamos como primeiro parametro "self" para entender que se trata do objeto que nos referenciamos naquele momento
