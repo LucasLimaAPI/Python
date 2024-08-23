8365f1dba99ade067684966a59c828b11a035c34
Lucas Lima de Oliveira <lllima3780@gmail.com>
2024-08-23 16:54:35 -0300

    Jogos

--- Jogos/adivinhacao.py
+++ Jogos/adivinhacao.py
@@ -4,12 +4,25 @@ print("*************************************")
 print("Bem vindo ao meu jogo de Adivinhação!")
 print("*************************************")
 
-numero_secreto = random.randrange(101)
-total_de_tentativas = 3
-print(numero_secreto)
+numero_secreto = random.randrange(1,101)
+total_de_tentativas = 0
+pontos = 1000
+
+print("Qual nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Díficil")
+nivel = int(input("Defina o Nível:"))
+
+if nivel == 1:
+  total_de_tentativas = 20
+elif nivel == 2:
+  total_de_tentativas = 10
+else:
+  total_de_tentativas = 5
+
 
 for rodada in range(1,total_de_tentativas + 1):
   print(f"Tentativa {rodada} de {total_de_tentativas}")
+
   palpite = int(input('Digite o seu número: '))
 
   if palpite < 1 or palpite > 100:
@@ -19,16 +32,16 @@ for rodada in range(1,total_de_tentativas + 1):
   acertou = numero_secreto == palpite
   maior = palpite > numero_secreto
   menor = palpite < numero_secreto
-  print("Você digitou", palpite)
 
   if acertou:
-    print("Você Acertou!!")
+    print(f"Você Acertou e fez {pontos} pontos")
     break
   else:
     if maior:
      print("Você errou, talvez seu número seja menor!")
-    else:
-      menor
+    elif menor :
       print("Você errou, talvez seu número seja maior!")
+      pontos_perdidos = abs(numero_secreto - palpite) #abs = absoluto
+      pontos = pontos - pontos_perdidos
 
 print("Fim de jogo.")
